ece415d1c1d7401253102d72012c5da63263bb23
Awais-niaz <awais.niaz@mwanmobile.org>
2023-01-27 21:27:30 +0500

    login credential implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    borders: {
+      'border-1': '1px'
+    },
     extend: {
       width: {
-        'w-100': '460px'
+        'w-100': '460px',
+        'w-4/4': '95%'
       }
     }
   },
