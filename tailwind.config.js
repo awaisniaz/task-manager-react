121b4cceecd5bed7911208e6ebc20585e1ac650a
Awais-niaz <awais.niaz@mwanmobile.org>
2023-01-27 17:12:47 +0500

    Login Screen Implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'w-100': '460px'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
